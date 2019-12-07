fc400ad7c46d60b6387fda4dde5d89a93a733385
cy4ok <cy4ok86@gmail.com>
2019-12-07 15:23:39 +0300

    Скопировал код из урока.

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 150 },
-    { title: 'Jacket', price: 150 },
-    { title: 'Shoes', price: 150 },
-];
+class GoodsItem {
+    constructor(title, price) {
+      this.title = title;
+      this.price = price;
+    }
+    render() {
+      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
 
-const renderGoodsItem = (title = '', price = '') =>  `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+class GoodsList {
+    constructor() {
+      this.goods = [];
+    }
+    fetchGoods() {
+      this.goods = [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+      ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+          const goodItem = new GoodsItem(good.title, good.price);
+          listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }    
+}
 
-const renderGoodsList = (list = []) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-};
-
-renderGoodsList(goods);
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
