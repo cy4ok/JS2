76469ea9e40ec6bb5a1f9d377677b1d6f4a01a29
cy4ok <cy4ok86@gmail.com>
2019-12-07 14:26:06 +0300

    Добавил значения по умолчанию для аргументов фунции и сократил запись функции.

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const goods = [
     { title: 'Shoes', price: 150 },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
-};
+const renderGoodsItem = (title = '', price = '') =>  `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 
-const renderGoodsList = (list) => {
+const renderGoodsList = (list = []) => {
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
 };
