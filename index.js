6135ae8fcde31babebe404a0299b003f16efe3a2
cy4ok <cy4ok86@gmail.com>
2019-12-16 23:48:33 +0300

    Написал промис. Выдает ошибку. Завтра разберусь.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-function makeGETRequest(url, callback) {
-    let xhr;
-    if (window.XMLHttpRequest) {
-        xhr = new window.XMLHttpRequest();
-    } else {
-        xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
-    }
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const body = JSON.parse(xhr.responseText);
-            callback(body)
+function makeGETRequest(url) {
+    return new Promise((resolve, reject) => {
+        let xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new window.XMLHttpRequest();
+        } else {
+            xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
         }
-    };
-    xhr.open('GET', url);
-    xhr.send();
-}
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const body = JSON.parse(xhr.responseText);
+                callback(body)
+            }
+        };
+        xhr.open('GET', url);
+        xhr.send();
+});
 
 class GoodsItem {
     constructor(title = 'Без имени', price = '') {
@@ -34,13 +35,12 @@ class GoodsList {
     constructor() {
         this.goods = [];
     }
-    fetchGoods(cb)  {
-        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-            this.goods = goods;
-            cb();
+    fetchGoods() {
+        makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+           return this.goods = goods;
         });
     }
-        sumItem() {
+    sumItem() {
         let summa = 0;
         this.goods.forEach(good => {summa += good.price});
         return summa;
