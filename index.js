f254ddcadb9057bd750c2807b0ff06fc303bcf76
cy4ok <cy4ok86@gmail.com>
2019-12-22 13:30:16 +0300

    Добавил обратно чат.

--- index.js
+++ index.js
@@ -45,65 +45,57 @@ const app = new Vue({
         }
     }
 });
-class GoodsItem {
-    constructor(title = 'Без имени', price = '') {
-        this.title = title;
-        this.price = price;
-    }
-    render() {
-        return `<div class="goods-item">
-                    <h3 class="title goods-title">${this.title}</h3>
-                    <p>${this.price} ₽</p>
-                </div>`;
-    }
-}
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    fetchGoods() {
-        return makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
-            this.goods = goods;
-        });
-    }
-    sumItem() {
-        let summa = 0;
-        this.goods.forEach(good => {
-            summa += good.price
-        });
-        return summa;
-    }
-    render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-    sumItems() {
-        let summa = 0;
-        this.goods.forEach(good => {
-            summa += good.price
-        });
-        return summa;
-    }
-}
-class Cart extends GoodsList {
-    constructor(props) {
-        super(props);
-    }
-    clean() {}
-    incGood() {}
-    decGood() {}
-}
-class CartItem extends GoodsItem {
-    constructor(props) {
-        super(props);
-    }
-    delete() {}
-}
-const list = new GoodsList();
-list.fetchGoods().then(() => {
-    list.render();
-});
+//class GoodsItem {
+//    constructor(title = 'Без имени', price = '') {
+//        this.title = title;
+//        this.price = price;
+//    }
+//    render() {
+//        return `<div class="goods-item">
+//                    <h3 class="title goods-title">${this.title}</h3>
+//                    <p>${this.price} ₽</p>
+//                </div>`;
+//    }
+//}
+//class GoodsList {
+//    constructor() {
+//        this.goods = [];
+//    }
+//    sumItem() {
+//        let summa = 0;
+//        this.goods.forEach(good => {
+//            summa += good.price
+//        });
+//        return summa;
+//    }
+//    render() {
+//        let listHtml = '';
+//        this.goods.forEach(good => {
+//            const goodItem = new GoodsItem(good.title, good.price);
+//            listHtml += goodItem.render();
+//        });
+//        document.querySelector('.goods-list').innerHTML = listHtml;
+//    }
+//    sumItems() {
+//        let summa = 0;
+//        this.goods.forEach(good => {
+//            summa += good.price
+//        });
+//        return summa;
+//    }
+//}
+//class Cart extends GoodsList {
+//    constructor(props) {
+//        super(props);
+//    }
+//    clean() {}
+//    incGood() {}
+//    decGood() {}
+//}
+//class CartItem extends GoodsItem {
+//    constructor(props) {
+//        super(props);
+//    }
+//    delete() {}
+//}
+//const list = new GoodsList();
