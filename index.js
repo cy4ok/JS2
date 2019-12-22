3972215bf9733f373c267389fda6be8028c9dfa8
cy4ok <cy4ok86@gmail.com>
2019-12-22 13:08:40 +0300

    Добавил vue.js

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
-function makeGETRequest(url) {
-    return new Promise((resolve, reject) => {
-        let xhr;
-        if (window.XMLHttpRequest) {
-            xhr = new window.XMLHttpRequest();
+const app = new Vue({
+    el: '#app'
+    , data: {
+        goods: []
+        , filteredGoods: []
+        , searchLine: ''
+    },
+    methods: {
+        makeGETRequest(url) {
+            return new Promise((resolve, reject) => {
+                let xhr;
+                if (window.XMLHttpRequest) {
+                    xhr = new window.XMLHttpRequest();
+                }
+                else {
+                    xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+                }
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            const body = JSON.parse(xhr.responseText);
+                            resolve(body)
+                        }
+                        else {
+                            reject(xhr.responseText);
+                        }
+                    }
+                };
+                xhr.onerror = function (err) {
+                    reject(err);
+                };
+                xhr.open('GET', url);
+                xhr.send();
+            });
         }
-        else {
-            xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+    }, 
+    async mounted() {
+        try {
+            this.goods = await this.makeGETRequest(`${API_URL}/catalogData.json`);
+            this.filteredGoods = [...this.goods];
         }
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const body = JSON.parse(xhr.responseText);
-                resolve(body);
-            }
-        };
-        xhr.onerror = function (err) {
-            reject(err);
+        catch (e) {
+            console.error(e);
         }
-        xhr.open('GET', url);
-        xhr.send();
-    });
-};
+    }
+});
 class GoodsItem {
     constructor(title = 'Без имени', price = '') {
         this.title = title;
@@ -53,27 +76,19 @@ class GoodsList {
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-          const goodItem = new GoodsItem(good.title, good.price);
-          listHtml += goodItem.render();
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
     }
     sumItems() {
         let summa = 0;
-        this.goods.forEach(good => {summa += good.price});
+        this.goods.forEach(good => {
+            summa += good.price
+        });
         return summa;
-    }     
-}
-
-class basket {
-    addBasket() {
-        //Метод добавляет в корзину товар.
-    }
-    removeBasket() {
-        //Метод удаляет товар из корзины.
     }
 }
-
 class Cart extends GoodsList {
     constructor(props) {
         super(props);
