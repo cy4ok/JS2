c2e666c46baedd70f04b9144802bfeeeb71d0538
cy4ok <cy4ok86@gmail.com>
2020-01-02 17:52:57 +0300

    Revert "Lesson 5"

--- index.js
+++ index.js
@@ -1,109 +1,60 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const app = new Vue({
-    el: '#app'
-    , data: {
-        goods: [] ,
-        filteredGoods: [] ,
-        searchLine: '' ,
-        isVisibleCart: false ,
+class GoodsItem {
+    constructor(title, price) {
+      this.title = title;
+      this.price = price;
     }
-    , methods: {
-        makeGETRequest(url) {
-                return new Promise((resolve, reject) => {
-                    let xhr;
-                    if (window.XMLHttpRequest) {
-                        xhr = new window.XMLHttpRequest();
-                    }
-                    else {
-                        xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
-                    }
-                    xhr.onreadystatechange = function () {
-                        if (xhr.readyState === 4) {
-                            if (xhr.status === 200) {
-                                const body = JSON.parse(xhr.responseText);
-                                resolve(body)
-                            }
-                            else {
-                                reject(xhr.responseText);
-                            }
-                        }
-                    };
-                    xhr.onerror = function (err) {
-                        reject(err);
-                    };
-                    xhr.open('GET', url);
-                    xhr.send();
-                });
-            },
-        filterGoods() {
-                const regexp = new RegExp(this.searchLine, 'i');
-                this.filteredGoods = this.goods.filter((good) => regexp.test(good.product_name));
-            },
-        getVisibleCart() {
-            this.isVisibleCart = !this.isVisibleCart;
-        },
+    render() {
+      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><button class="basket">Купить</button></div>`;
     }
-    , async mounted() {
-        try {
-            this.goods = await this.makeGETRequest(`${API_URL}/catalogData.json`);
-            this.filteredGoods = [...this.goods];
-        }
-        catch (e) {
-            console.error(e);
-        }
+}
+
+class GoodsList {
+    constructor() {
+      this.goods = [];
     }
-});
-//class GoodsItem {
-//    constructor(title = 'Без имени', price = '') {
-//        this.title = title;
-//        this.price = price;
-//    }
-//    render() {
-//        return `<div class="goods-item">
-//                    <h3 class="title goods-title">${this.title}</h3>
-//                    <p>${this.price} ₽</p>
-//                </div>`;
-//    }
-//}
-//class GoodsList {
-//    constructor() {
-//        this.goods = [];
-//    }
-//    sumItem() {
-//        let summa = 0;
-//        this.goods.forEach(good => {
-//            summa += good.price
-//        });
-//        return summa;
-//    }
-//    render() {
-//        let listHtml = '';
-//        this.goods.forEach(good => {
-//            const goodItem = new GoodsItem(good.title, good.price);
-//            listHtml += goodItem.render();
-//        });
-//        document.querySelector('.goods-list').innerHTML = listHtml;
-//    }
-//    sumItems() {
-//        let summa = 0;
-//        this.goods.forEach(good => {
-//            summa += good.price
-//        });
-//        return summa;
-//    }
-//}
-//class Cart extends GoodsList {
-//    constructor(props) {
-//        super(props);
-//    }
-//    clean() {}
-//    incGood() {}
-//    decGood() {}
-//}
-//class CartItem extends GoodsItem {
-//    constructor(props) {
-//        super(props);
-//    }
-//    delete() {}
-//}
-//const list = new GoodsList();
+    fetchGoods() {
+      this.goods = [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+      ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+          const goodItem = new GoodsItem(good.title, good.price);
+          listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    sumItems() {
+        let summa = 0;
+        this.goods.forEach(good => {summa += good.price});
+        return summa;
+    }     
+}
+
+class basket {
+    addBasket() {
+        //Метод добавляет в корзину товар.
+    }
+    removeBasket() {
+        //Метод удаляет товар из корзины.
+    }
+}
+
+class basketItems {
+    allItemsBasket() {
+        //Метод отображает все товары в корзине.
+    }
+    sumItemsBasket () {
+        //Метод считает сумму товаров в корзине.
+    }
+}
+
+const list = new GoodsList();
+
+list.fetchGoods();
+list.render();
+list.sumItems();
